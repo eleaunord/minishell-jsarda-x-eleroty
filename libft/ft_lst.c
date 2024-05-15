3c51436a84df1f2f19bb20fc07eab6e7da7b5c3f
Eleonore Roty <eleroty@paul-f3Ar1s2.clusters.42paris.fr>
2024-05-15 17:00:57 +0200

    New temporary branch
    This is a new branch to work on and share our progress. I just updated the libft file so I could push the branch.
    I will push my tokenization files later on on it but you can have a look direclty on my own branch in the meantime if you want!

---
+++ libft/ft_lst.c
@@ -0,0 +1,59 @@
+#include "libft.h"
+
+void	ft_putendl_fd(char const *s, int fd)
+{
+	int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+		i++;
+	write(fd, s, i);
+	write(fd, "\n", 1);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *current;
+	t_list *tmp;
+
+	if (!lst || !del)
+		return ;
+	current = *lst;
+	while (current != NULL)
+	{
+		tmp = current;
+		current = current->next;
+		(del)(tmp->content);
+		(free)(tmp);
+	}
+	*lst = NULL;
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (NULL == lst)
+		return ;
+	if (NULL == *lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (node == NULL)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
---
+++ libft/ft_substr.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new_s;
+	size_t	s_length;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	s_length = ft_strlen(s);
+	if (s_length <= len + start)
+		len = s_length - start;
+	if (s_length < start)
+	{
+		new_s = malloc(1);
+		new_s[0] = '\0';
+		return (new_s);
+	}
+	new_s = (char *)malloc((sizeof(*s) * (len + 1)));
+	if (!new_s)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+		new_s[i++] = s[start++];
+	new_s[i] = '\0';
+	return (new_s);
+}
