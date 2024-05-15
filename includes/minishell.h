adb6c9f001b19b955c3ac3a5832a47deb5ad6964
Julien Sarda <jsarda@paul-f3Ar2s2.clusters.42paris.fr>
2024-05-15 16:11:30 +0200

    FIX ISSUE
    i fixed the issue where i didnt push the whole folder

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsarda <jsarda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 13:49:07 by jsarda            #+#    #+#             */
+/*   Updated: 2024/05/15 15:30:39 by jsarda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
