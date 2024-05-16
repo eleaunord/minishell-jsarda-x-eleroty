5750afc4de116b4d75b8adfc0e765db84c54a1b3
Julien Sarda <jsarda@bess-f2r2s16.clusters.42paris.fr>
2024-05-16 10:45:27 +0200

    Finish simple command
    i finish to implement the simple command with execve et fork
    i started to work on the redirection

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jsarda <jsarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 13:49:07 by jsarda            #+#    #+#             */
-/*   Updated: 2024/05/15 15:30:39 by jsarda           ###   ########.fr       */
+/*   Updated: 2024/05/16 10:44:30 by jsarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
+#include <fcntl.h>
 
 #endif
