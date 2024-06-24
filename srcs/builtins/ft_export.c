52c62542e1488f7a69ade7f9fa22b0466c795091
Julien Sarda <jsarda@bess-f2r4s4.clusters.42paris.fr>
2024-06-24 13:57:50 +0200

    added export

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -6,47 +6,28 @@
 /*   By: jsarda <jsarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 09:24:48 by jsarda            #+#    #+#             */
-/*   Updated: 2024/06/24 09:06:41 by jsarda           ###   ########.fr       */
+/*   Updated: 2024/06/24 13:54:39 by jsarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	modify_value(t_env *current, t_env *current_duplicate,
-		const char *value)
+void	modify_value(t_env *env, const char *value)
 {
-	free(current->value);
-	free(current_duplicate->value);
-	current->value = ft_strdup(value);
-	current_duplicate->value = ft_strdup(value);
-	if (!current->value && !current_duplicate->value)
+	free(env->value);
+	env->value = ft_strdup(value);
+	if (!env->value)
 	{
 		perror("strdup");
 		return ;
 	}
-	free(current);
-	free(current_duplicate);
-	current = malloc(ft_strlen(current->key) + ft_strlen(current->value) + 2);
-	current_duplicate = malloc(ft_strlen(current->key)
-			+ ft_strlen(current->value) + 2);
-	if (!current && !current_duplicate)
-	{
-		perror("malloc");
-		return ;
-	}
-	sprintf(current, "%s=%s", current->key, current->value);
-	sprintf(current_duplicate, "%s=%s", current->key, current->value);
 }
 
-void	create_var(t_minishell *data, const char *key, const char *value)
+void	create_var(t_env *env, const char *key, const char *value)
 {
 	t_env	*new_var;
 	t_env	*current;
-	size_t	value_size;
-	size_t	key_size;
 
-	key_size = 0;
-	value_size = 0;
 	new_var = malloc(sizeof(t_env));
 	if (!new_var)
 	{
@@ -70,31 +51,66 @@ void	create_var(t_minishell *data, const char *key, const char *value)
 			free(new_var);
 			return ;
 		}
-		value_size = ft_strlen(value);
 	}
 	else
 		new_var->value = NULL;
-	key_size = ft_strlen(key);
-	new_var->str = malloc(key_size + value_size + 2);
-	if (!new_var->str)
-	{
-		perror("malloc");
-		free(new_var->key);
-		free(new_var->value);
-		free(new_var);
-		return ;
-	}
-	if (data->env == NULL)
+	if (env == NULL)
 		new_var->next = new_var;
 	else
 	{
-		current = data->env;
-		while (current->next != data->env)
+		current = env;
+		while (current->next != env)
 			current = current->next;
 		current->next = new_var;
-		new_var->next = data->env;
+		new_var->next = env;
 	}
-	data->env = new_var;
+	env = new_var;
+}
+
+void	sort_ascii(t_env *env)
+{
+	int		swapped;
+	t_env	*ptr1;
+	t_env	*lptr;
+	t_env	*start;
+	char	*temp_key;
+	char	*temp_value;
+
+	lptr = NULL;
+	start = env;
+	if (env == NULL)
+		return ;
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		ptr1 = start;
+		while (ptr1->next != lptr)
+		{
+			if (strcmp(ptr1->key, ptr1->next->key) > 0)
+			{
+				temp_key = ptr1->key;
+				temp_value = ptr1->value;
+				ptr1->key = ptr1->next->key;
+				ptr1->value = ptr1->next->value;
+				ptr1->next->key = temp_key;
+				ptr1->next->value = temp_value;
+				swapped = 1;
+			}
+			ptr1 = ptr1->next;
+			if (ptr1 == start)
+				break ;
+		}
+		lptr = ptr1;
+	}
+}
+
+void	ft_print_export(t_env *env_dup)
+{
+	if (!env_dup)
+		return ; // need to add pwd , shlvl , _
+	sort_ascii(env_dup);
+	print_env(env_dup);
 }
 
 void	ft_export(t_minishell *data, t_node *node, char **args)
@@ -104,6 +120,7 @@ void	ft_export(t_minishell *data, t_node *node, char **args)
 	t_env	*current_duplicate;
 	char	**var;
 
+	(void)node;
 	if (!args || !data)
 		return ;
 	i = 1;
@@ -122,19 +139,37 @@ void	ft_export(t_minishell *data, t_node *node, char **args)
 			continue ;
 		}
 		current = data->env;
-		current_duplicate = data->env_dup;
 		while (current)
 		{
 			if (ft_strcmp(current->key, var[0]) == 0)
 			{
-				modify_value(current, current_duplicate, var[1]);
+				if (!var[1])
+					break ;
+				modify_value(current, var[1]);
 				break ;
 			}
 			current = current->next;
 			if (current == data->env)
 			{
 				if (ft_strchr(args[i], '='))
-					create_var(data, var[0], var[1]);
+					create_var(current, var[0], var[1]);
+				break ;
+			}
+		}
+		current_duplicate = data->env_dup;
+		while (current_duplicate)
+		{
+			if (ft_strcmp(current_duplicate->key, var[0]) == 0)
+			{
+				if (!var[1])
+					break ;
+				modify_value(current_duplicate, var[1]);
+				break ;
+			}
+			current_duplicate = current_duplicate->next;
+			if (current_duplicate == data->env_dup)
+			{
+				create_var(current_duplicate, var[0], var[1]);
 				break ;
 			}
 		}
